115a7f5da2fa7d76e7db9ebf5f3abc00a19a8afe
Edgar <edgar@alfredit.net>
2025-04-11 23:10:47 +0200

    add brightness control

--- app.php
+++ app.php
@@ -6,21 +6,33 @@ error_reporting(E_ALL);
 require_once 'settings.php';
 require_once 'fonctions.php';
 
+$brightness = '41';
+
 if (isset($_GET["message"])) {
 
-	if ($_GET["message"] == "refreshtv") {
+$parts = explode("-", $_GET["message"]);
+$message = $parts[0];
+
+if ($parts[1] > 0) {
+$brightness = (int)$parts[1];
+}
+echo "brightness : ".$brightness." (41 is the default value)<br>";
+
+	if ($message == "refreshtv") {
+		echo "refresh tv<br>";
 		kill_process();
 		get_ha_image($ha_url_tv, $long_lived_access_token);
-		display_image($folder);
+		display_image($folder,$brightness);
 	}
 
-        if ($_GET["message"] == "refreshmusic") {
+        if ($message == "refreshmusic") {
+                echo "refresh music<br>";
                 kill_process();
                 get_ha_image($ha_url_music, $long_lived_access_token);
-                display_image($folder);
+                display_image($folder,$brightness);
         }
 
-        if ($_GET["message"] == "kill") {
+        if ($message == "kill") {
                 kill_process();
         }
 }
